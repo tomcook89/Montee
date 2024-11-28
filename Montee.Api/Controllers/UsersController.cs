7420c7b76da0be05b3764885fb885129f9bf8610
tomcook89 <thomas_cook89@yahoo.com>
2024-11-28 22:25:01 +0000

    Add database and users controller
    Add database and users controller

---
+++ Montee.Api/Controllers/UsersController.cs
@@ -0,0 +1,30 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Montee.Domain.Models;
+using Montee.Infra.Data.Context;
+
+namespace Montee.Api.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class UsersController(DBContext context) : ControllerBase
+{
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<AppUser>> GetUsers()
+    {
+        var users = await context.Users.ToListAsync();
+
+        return users;
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<AppUser>> GetUser(int id)
+    {
+        var currentUsername = User;
+        var user = await context.Users.FindAsync(id);
+
+        if (user == null) return NotFound();
+
+        return user;
+    }
+}

4fc20ff6c26dbab8208025b4232766312d8b62fb
tomcook89 <thomas_cook89@yahoo.com>
2024-12-12 12:23:35 +0000

    Persist login

--- Montee.Api/Controllers/AccountController.cs
+++ Montee.Api/Controllers/AccountController.cs
@@ -56,6 +56,15 @@ public class AccountController(DBContext context, ITokenService tokenService //U
 
         if (user == null || user.UserName == null) return Unauthorized("Invalid username");
 
+        using var hmac = new HMACSHA512(user.PasswordSalt);
+
+        var computedHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(loginDto.Password));
+
+        for (int i = 0; i < computedHash.Length; i++)
+        {
+            if (computedHash[i] != user.PasswordHash[i]) return Unauthorized("Invalid password");
+        }
+
         return new UserDto
         {
             Username = user.UserName,
--- Montee.client/src/app/app.component.ts
+++ Montee.client/src/app/app.component.ts
@@ -3,6 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { Component, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavComponent } from "./nav/nav.component";
+import { AccountsService } from './_services/accounts.service';
 
 @Component({
   selector: 'app-root',
@@ -13,10 +14,23 @@ import { NavComponent } from "./nav/nav.component";
 })
 export class AppComponent implements OnInit{
   http = inject(HttpClient);
+  private accountService = inject(AccountsService)
   title = 'Montee.client';
   users: any;
 
   ngOnInit(): void {
+    this.getUsers();
+    this.setCurrentUser();
+  }
+
+  setCurrentUser(){
+    const userString = localStorage.getItem('user');
+    if(!userString) return;
+    const user = JSON.parse(userString);
+    this.accountService.currentUser.set(user);
+  }
+
+  getUsers(){
     this.http.get('/api/users').subscribe({
       next: response => this.users = response,
       error: error => console.log(error),

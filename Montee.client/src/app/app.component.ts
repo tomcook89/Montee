d24f9350eb6fbda4df53951ae018aa2090880dd3
tomcook89 <thomas_cook89@yahoo.com>
2025-03-26 10:39:52 +0000

    Add footer component

--- Montee.client/src/app/app.component.ts
+++ Montee.client/src/app/app.component.ts
@@ -1,14 +1,13 @@
-import { NgFor } from '@angular/common';
 import { Component, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavComponent } from "./nav/nav.component";
 import { AccountService } from './_services/account.service';
-import { HomeComponent } from "./home/home.component";
+import { FooterComponent } from './footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NgFor, NavComponent, HomeComponent],
+  imports: [RouterOutlet, NavComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
